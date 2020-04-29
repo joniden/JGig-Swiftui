d4a1687d08109f2add757c86efa3578ec6646668
Joacim Niden <jocke@niden.se>
2020-04-29 11:53:23 +0200

    Initial Commit

---
+++ JGig-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JGig-SwiftUI
+//
+//  Created by Joacim Nidén on 2020-04-29.
+//  Copyright © 2020 Joacim Nidén. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
